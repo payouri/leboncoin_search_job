51246c2f3da46bd4ff95fe9ac04ef20b130fd42a
Pailloux Youri <pailloux.youri@gmail.com>
2020-07-06 20:32:01 +0200

    include build cause of heroku timeout

---
+++ build/precache-manifest.0919b3a7c54d4c49866004b9d1386e7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8d7e9f5f2272c3f854fa0e98fed7da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "458d37b42073b9221153",
+    "url": "/static/css/2.c27a359e.chunk.css"
+  },
+  {
+    "revision": "4110eadea1a61693ae9f",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "458d37b42073b9221153",
+    "url": "/static/js/2.20d93eee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.20d93eee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b531287a5d8e4a9d718",
+    "url": "/static/js/3.57021ff9.chunk.js"
+  },
+  {
+    "revision": "8b49862136f4eeb0618e",
+    "url": "/static/js/4.933d5b2e.chunk.js"
+  },
+  {
+    "revision": "4110eadea1a61693ae9f",
+    "url": "/static/js/main.0cb5c603.chunk.js"
+  },
+  {
+    "revision": "76eea5c9f8619aa224cb",
+    "url": "/static/js/runtime-main.18ee2283.js"
+  }
+]);
